0379613317dadcde3de985a3eb852669d53fd677
cooklee <sbbogus@gmail.com>
2021-04-09 11:42:03 +0200

    remove localsettings

--- shorted_url/local_settings.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'django-insecure-zxupm*#(m_i$cclqp^yc0*h&3$vxv8k@(0@mtxu*^t*4qwv395'
--- shorted_url/local_settings_temp.py
+++ shorted_url/local_settings_temp.py
@@ -1,1 +1,3 @@
 SECRET_KEY = '' #create your onw secret key
+SECRET_KEY = 'django-insecure-zxupm*#(m_i$cclqp^yc0*h&3$vxv8k@(0@mtxu*^t*4qwv395'
+ALLOWED_HOSTS = ['shorted.pythonanywhere.com']
